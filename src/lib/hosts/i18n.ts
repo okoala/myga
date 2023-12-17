404e19e61e358998a5c187d5afe7217b320a1c2e
okoala <dapixp@gmail.com>
2023-12-17 16:54:07 +0800

    feat: 优化语言判断

--- src/lib/hosts/i18n.ts
+++ src/lib/hosts/i18n.ts
@@ -1,4 +1,4 @@
 export async function detectLanguage(text: string): Promise<string> {
-  let result = await chrome.i18n.detectLanguage(text);
-  return result.languages[0].language;
+  let result = await chrome.i18n?.detectLanguage(text);
+  return result ? result.languages[0].language : 'auto';
 }
