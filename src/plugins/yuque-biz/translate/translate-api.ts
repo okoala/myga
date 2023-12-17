8b9f3e599aacbc5f2691261fe16155e83aabd604
okoala <dapixp@gmail.com>
2023-12-17 17:04:29 +0800

    feat: 加强翻译结果

--- src/plugins/yuque-biz/translate/translate-api.ts
+++ src/plugins/yuque-biz/translate/translate-api.ts
@@ -3,7 +3,20 @@ import { detectLanguage } from '@lib/hosts/i18n';
 
 export class TranslateApi {
   async text(text: string) {
-    const language = await detectLanguage(text);
+    let result;
+
+    try {
+      const language = await detectLanguage(text);
+      result = await this._translateText(text, language);
+    } catch (err) {
+      // 如果失败就尝试一次 auto
+      result = await this._translateText(text, 'auto');
+    }
+
+    return result;
+  }
+
+  async _translateText(text: string, language: string) {
     const res: any = await request({
       url: `/api/services/translate/text`,
       config: {
